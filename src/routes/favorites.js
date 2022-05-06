f35d36f8e6f4ef4e69635a06860cfca8a46276ad
Cor4zon <V.Fedorov@bellerage.com>
2022-05-06 14:23:48 +0300

    add Favorites characters initial version

--- src/routes/favorites.js
+++ src/routes/favorites.js
@@ -1,10 +1,22 @@
 import React from "react";
 import App from "../App";
+import CardGrid from "../components/CardGrid/CardGrid";
+
+import { characters } from "../charactersData";
+import Card from "../components/Card/Card";
 
 const Favorites = () => {
+  const favoriteCharacters = JSON.parse(
+    localStorage.getItem("favoriteCharacters")
+  );
+
+  const cards = characters.filter((character, id) =>
+    favoriteCharacters.includes(id+1)
+  );
+
   return (
     <div>
-      <h2>Favorites</h2>
+      <CardGrid characters={ cards }/>
     </div>
   );
 };
